84c0899ba91e73a3d143d8387f016da1962aafbf
John Sok <98001557+jsok59@users.noreply.github.com>
2025-04-13 16:46:10 -0400

    Create eventlisteners for dialog open/close and form submit for project

--- src/index.js
+++ src/index.js
@@ -2,9 +2,31 @@ import { createProject, createTodo, projectList } from "./logic.js";
 import { renderSidebar } from "./renderSidebar.js";
 import "./style.css";
 
-const createProjectButton = document.querySelector('.create-project');
+renderSidebar();
+
+//Open/close Project Dialog Button
+const createProjectDialogButton = document.querySelector('.create-project');
+const closeProjectButton = document.querySelector('.project-dialog .close');
 const projectDialog = document.querySelector('.project-dialog');
-createProjectButton.addEventListener('click', () => projectDialog.showModal());
+createProjectDialogButton.addEventListener('click', () => projectDialog.showModal());
+closeProjectButton.addEventListener('click', () => projectDialog.close())
+
+
+//Open/close Todo Dialog Button
+const createTodoDialogButton = document.querySelector('.create-todo');
+const closeTodoButton = document.querySelector('.todo-dialog .close');
+const todoDialog = document.querySelector('.todo-dialog');
+createTodoDialogButton.addEventListener('click', () => todoDialog.showModal());
+closeTodoButton.addEventListener('click', () => todoDialog.close())
+
+//Form submit Project
+
+const projectForm = document.querySelector(".project-dialog > form");
+projectForm.addEventListener('submit', () => {
+    projectList.addProject(createProject(projectForm.title.value, projectForm.desc.value,[]));
+    renderSidebar();
+})
+
 
 
 window.debug = {
